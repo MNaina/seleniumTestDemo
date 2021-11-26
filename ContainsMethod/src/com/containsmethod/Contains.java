475401a1b8da8b116b2818bad6367bd192a10944
MNaina <nainam.selenium@gmail.com>
2021-11-26 11:05:51 +0530

    dd

---
+++ ContainsMethod/src/com/containsmethod/Contains.java
@@ -0,0 +1,13 @@
+package com.containsmethod;
+
+public class Contains {
+
+	public static void main(String[] args) {
+		String s1 = "iqst technologies";
+		
+		
+		System.out.println(s1.contains("iqst"));
+		System.out.println(s1.contains("naina"));
+
+}
+}
